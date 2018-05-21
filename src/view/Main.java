eadaa51f6d459540abe19f7e2cc3e07799291e7e
Dillon J <dillon.p.johnson@gmail.com>
2018-05-21 16:14:42 -0700

    dev

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,6 +1,8 @@
 package view;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,29 +12,41 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent> {
+
+	Button button;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("financeTracker.fxml"));
         primaryStage.setTitle("Finance Tracker");
 
-
-        Button button = new Button();
-        button.setText("click me");
+        button = new Button();
+        button.setText("Create User");
         button.setLayoutX(25);
         button.setLayoutY(25);
+        button.setOnAction(this);
 
 		Pane layout = new Pane();
 		layout.getChildren().add(button);
 
 		primaryStage.setScene(new Scene(layout, 800, 600));
 
+
+		//Starting application
 		primaryStage.show();
+		Controller controller = new Controller();
     }
 
     public static void main(String[] args) {
         launch(args);
 
     }
+
+	@Override
+	public void handle(ActionEvent event) {
+		if (event.getSource() == button) {
+			System.out.println("hello");
+		}
+	}
 }
